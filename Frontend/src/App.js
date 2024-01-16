53e2cb8510e22cbdde2ffb39fc760838e780a30f
alewis3421 <60369233+alewis3421@users.noreply.github.com>
2024-01-16 02:12:40 -0600

    Set up node for site

---
+++ Frontend/src/App.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    
+                </Routes>
+            </Router>
+        </div>
+    )
+}
