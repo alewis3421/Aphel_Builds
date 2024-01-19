9dfa9d3a8835f5cf306118776d253fcb0d8960f8
alewis3421 <60369233+alewis3421@users.noreply.github.com>
2024-01-19 17:09:39 -0600

    Finished setting up the header
    will eventually change the "logo" to an actual one with some design to it

--- Frontend/src/components/HomePage.js
+++ Frontend/src/components/HomePage.js
@@ -5,8 +5,6 @@ export class Homepage extends Component {
     render() {
         return (
             <div className="homepage">
-                <h1>Homepage</h1>
-                please just display
             </div>
         );
     }
--- Frontend/src/components/header.js
+++ Frontend/src/components/header.js
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 import './header.css';
 
 export class Header extends Component {
+    componentDidMount() {
+        const link1 = document.createElement('link');
+        link1.rel = 'preconnect';
+        link1.href = 'https://fonts.googleapis.com';
+    
+        const link2 = document.createElement('link');
+        link2.rel = 'preconnect';
+        link2.href = 'https://fonts.gstatic.com';
+        link2.crossOrigin = 'true';
+    
+        const link3 = document.createElement('link');
+        link3.href = "https://fonts.googleapis.com/css2?family=Teko&display=swap" ;
+        link3.rel = 'stylesheet';
+       
+        document.head.appendChild(link1);
+        document.head.appendChild(link2);
+        document.head.appendChild(link3);
+    }
     render() {
         return (
             <div className='header'>
-                Aphel Builds
+                <h1><a href="/" id="title">Aphel Builds</a></h1>
+                <nav>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">Builder</a></li>
+                        <li><a href="/">Pre-Builts</a></li>
+                        <li><a href="/">Sign In</a></li>
+                    </ul>
+                </nav>     
             </div>
         )
     }
