9fe9514d87cb8a387739669c37cb76143d41ed7c
alewis3421 <60369233+alewis3421@users.noreply.github.com>
2024-01-19 22:56:35 -0600

    put in actual work on the homepage
    need to think about the design a bit more, feels rather basic at the moment

--- Frontend/src/components/HomePage.js
+++ Frontend/src/components/HomePage.js
@@ -4,7 +4,19 @@ import './homepage.css';
 export class Homepage extends Component {
     render() {
         return (
-            <div className="homepage">
+            <div className="homepage vert-center">
+                lets git it going 
+                <div className='title'>
+                Welcome to<br></br>
+                <h2 id='title2'>Aphel Builds</h2>
+                Don't want to build youre own pc well you've come to the right place <br/><br/>
+                Want to build your own pc but want a place to build a list then you've also come to the right place<br/>
+                
+                <button></button>
+                </div>
+                <div className='builds'>
+                    
+                </div>
             </div>
         );
     }
